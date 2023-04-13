3f0c10414d16d7b3cbfc5c350b298b1f080bd375
carvalheirafc <carvalheirafc@gmail.com>
2023-04-13 20:02:38 -0300

    cleaned files and added some configs

--- base/base_trainer.py
+++
@@ -1,9 +0,0 @@
-class BaseTrain(object):
-    def __init__(self, model, train_dataset, validation_dataset, config):
-        self.model = model
-        self.train_dataset = train_dataset
-        self.validation_dataset = validation_dataset
-        self.config = config
-
-    def train(self):
-        raise NotImplementedError
